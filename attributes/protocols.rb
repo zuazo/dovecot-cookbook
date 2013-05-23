51f03dc281867224114249144a9fc49dcb3f29e6
Xabier de Zuazo <xabier@onddo.com>
2013-05-23 17:39:11 +0200

    20-lmtp.conf.erb template using variables

--- attributes/protocols.rb
+++ attributes/protocols.rb
@@ -1,9 +1,10 @@
 
 default['dovecot']['protocols'] = {}
 
+default['dovecot']['protocols']['lda'] = nil
 # default['dovecot']['protocols']['lda'] = {
 #   'mail_plugins' => [ '$mail_plugins' ],
 # }
-
 default['dovecot']['protocols']['imap'] = {}
+default['dovecot']['protocols']['lmtp'] = nil
 
