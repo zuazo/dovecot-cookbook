0ef7aa4a34b0b1d101a02a4430d1cfb2afd09296
Xabier de Zuazo <xabier@onddo.com>
2013-05-23 17:50:52 +0200

    90-acl.conf.erb template using variables

--- attributes/plugins.rb
+++ attributes/plugins.rb
@@ -3,8 +3,10 @@ default['dovecot']['mail_plugins'] = [ 'sieve' ]
 
 default['dovecot']['plugins']['sieve'] = {}
 
+default['dovecot']['plugins']['mail_log'] = nil
 # default['dovecot']['plugins']['mail_log'] = {
 #   'mail_log_events' => 'delete undelete expunge copy mailbox_delete mailbox_rename',
 #   'mail_log_fields' => 'uid box msgid size'
 # }
+default['dovecot']['plugins']['acl'] = nil
 
