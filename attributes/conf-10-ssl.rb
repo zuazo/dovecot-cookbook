fdd53a48cc4500bca1b0a3e278cdc578471b2aba
Xabier de Zuazo <xabier@onddo.com>
2013-05-23 17:11:31 +0200

    10-ssl.conf.erb template using variables

---
+++ attributes/conf-10-ssl.rb
@@ -0,0 +1,14 @@
+
+default['dovecot']['conf']['ssl_cert'] = '</etc/ssl/certs/dovecot.pem'
+default['dovecot']['conf']['ssl_key'] = '</etc/ssl/private/dovecot.pem'
+
+default['dovecot']['conf']['ssl'] = nil
+default['dovecot']['conf']['ssl_cert'] = nil
+default['dovecot']['conf']['ssl_key'] = nil
+default['dovecot']['conf']['ssl_key_password'] = nil
+default['dovecot']['conf']['ssl_ca'] = nil
+default['dovecot']['conf']['ssl_verify_client_cert'] = nil
+default['dovecot']['conf']['ssl_cert_username_field'] = nil
+default['dovecot']['conf']['ssl_parameters_regenerate'] = nil
+default['dovecot']['conf']['ssl_cipher_list'] = nil
+
