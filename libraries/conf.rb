c7694c565598e2cb29bfeea1a463dd8fcd261412
Xabier de Zuazo <xabier@onddo.com>
2013-05-23 11:18:49 +0200

    Created service formatting library function

--- libraries/conf.rb
+++ libraries/conf.rb
@@ -1,4 +1,6 @@
 
+require 'erubis'
+
 module Dovecot
   module Conf
 
@@ -12,6 +14,40 @@ module Dovecot
       else
         v.to_s
       end
+    end
+
+    def self.service(name, conf)
+
+      template =
+'service <%= @name %> {
+  <% if @conf["listeners"].kind_of?(Array)
+      @conf["listeners"].each do |listener|
+        listener.each do |service, values|
+          service_proto = service.split(":")[0]
+          service_name = service.split(":")[1]
+  -%>
+  <%= service_proto %>_listener <%= service_name %> {
+  <%     values.each do |key, value|-%>
+    <%= key %> = <%= @Dovecot_Conf.value(value) %>
+  <%     end -%>
+  }
+  <%     end -%>
+  <%   end -%>
+  <% end -%>
+  <% @conf.each do |key, value|
+      if key != "listeners"
+  -%>
+  <%= key %> = <%= @Dovecot_Conf.value(value) %>
+  <%   end -%>
+  <% end -%>
+}'
+
+      eruby = Erubis::Eruby.new(template)
+      eruby.evaluate(
+        :name => name,
+        :conf => conf,
+        :Dovecot_Conf => Dovecot::Conf
+      )
     end
 
   end
