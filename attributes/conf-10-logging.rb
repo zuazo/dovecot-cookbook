f57f47b4cc929c98673dcd3fa5c519de91330145
Xabier de Zuazo <xabier@onddo.com>
2013-05-23 13:27:13 +0200

    added default attribute values for 10-logging.conf.erb

---
+++ attributes/conf-10-logging.rb
@@ -0,0 +1,17 @@
+
+default['dovecot']['log_path'] = nil
+default['dovecot']['info_log_path'] = nil
+default['dovecot']['debug_log_path'] = nil
+default['dovecot']['syslog_facility'] = nil
+default['dovecot']['auth_verbose'] = nil
+default['dovecot']['auth_verbose_passwords'] = nil
+default['dovecot']['auth_debug'] = nil
+default['dovecot']['auth_debug_passwords'] = nil
+default['dovecot']['mail_debug'] = nil
+default['dovecot']['verbose_ssl'] = nil
+default['dovecot']['log_timestamp'] = nil
+default['dovecot']['login_log_format_elements'] = nil
+default['dovecot']['login_log_format'] = nil
+default['dovecot']['mail_log_prefix'] = nil
+default['dovecot']['deliver_log_format'] = nil
+
