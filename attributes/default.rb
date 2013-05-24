70cd28328ec1655d0bf54390bf68ca4b3831cca0
Xabier de Zuazo <xabier@onddo.com>
2013-05-24 10:51:57 +0200

    lib_path attribute added

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,11 @@
 default['dovecot']['user'] = 'dovecot'
 default['dovecot']['group'] = node['dovecot']['user']
 
+case node['platform']
+  when 'redhat','centos','scientific','fedora','suse','amazon' then
+    default['dovecot']['lib_path'] = '/usr/libexec/dovecot'
+  # when 'debian', 'ubuntu' then
+  else
+    default['dovecot']['lib_path'] = '/usr/lib/dovecot'
+end
+
