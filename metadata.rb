510549fadba9414bca8ff3e1d5a51e1cf6c6095a
Xabier de Zuazo <xabier@onddo.com>
2013-05-25 14:04:53 +0200

    metadata: added ubuntu, centos and debian support

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,8 @@ license          'Apache 2.0'
 description      'Installs/Configures dovecot'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+supports 'centos'
+supports 'debian'
+supports 'ubuntu'
+
