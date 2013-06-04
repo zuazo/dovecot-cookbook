48ffa5214938c9f412f9712a77a0bcff2df1d1b8
Xabier de Zuazo <xabier@onddo.com>
2013-06-04 21:31:40 +0200

    added final dot to all descriptions

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'dovecot'
 maintainer       'Onddo Labs, Sl.'
 maintainer_email 'team@onddo.com'
 license          'Apache 2.0'
-description      'Installs/Configures dovecot'
+description      'Installs and configures dovecot.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
@@ -12,56 +12,56 @@ supports 'ubuntu', '>= 12.04'
 
 attribute 'dovecot/user',
   :display_name => 'dovecot user',
-  :description => 'Dovector system user. Should no be changed',
+  :description => 'Dovector system user. Should no be changed.',
   :type => 'string',
   :required => 'optional',
   :default => 'dovecot'
 
 attribute 'dovecot/group',
   :display_name => 'dovecot group',
-  :description => 'Dovector system group. Should no be changed',
+  :description => 'Dovector system group. Should no be changed.',
   :type => 'string',
   :required => 'optional',
   :default => 'dovecot'
 
 attribute 'dovecot/lib_path',
   :display_name => 'dovecot library path',
-  :description => 'Dovector library path. Should no be changed',
+  :description => 'Dovector library path. Should no be changed.',
   :calculated => true,
   :type => 'string',
   :required => 'optional'
 
 attribute 'dovecot/conf_path',
   :display_name => 'dovecot configuration path',
-  :description => 'Dovector configruration files path. Should no be changed',
+  :description => 'Dovector configruration files path. Should no be changed.',
   :type => 'string',
   :required => 'optional',
   :default => '/etc/dovecot'
 
 attribute 'dovecot/conf_files_user',
   :display_name => 'dovecot configuration files user',
-  :description => 'System user owner of configuration files',
+  :description => 'System user owner of configuration files.',
   :type => 'string',
   :required => 'optional',
   :default => 'root'
 
 attribute 'dovecot/conf_files_group',
   :display_name => 'dovecot configuration files group',
-  :description => 'System group owner of configuration files',
+  :description => 'System group owner of configuration files.',
   :type => 'string',
   :required => 'optional',
   :default => 'node["dovecot"]["group"]'
 
 attribute 'dovecot/conf_files_mode',
   :display_name => 'dovecot configuration files mode',
-  :description => 'Configuration files system file mode bits',
+  :description => 'Configuration files system file mode bits.',
   :type => 'string',
   :required => 'optional',
   :default => '00644'
 
 attribute 'dovecot/conf_files/core',
   :display_name => 'dovecot core configuration files',
-  :description => 'Dovecot core configuration files list',
+  :description => 'Dovecot core configuration files list.',
   :type => 'array',
   :required => 'optional',
   :default => [
@@ -93,7 +93,7 @@ attribute 'dovecot/conf_files/core',
 
 attribute 'dovecot/conf_files/imap',
   :display_name => 'dovecot imap configuration files',
-  :description => 'Dovecot IMAP configuration files list',
+  :description => 'Dovecot IMAP configuration files list.',
   :type => 'array',
   :required => 'optional',
   :default => [
@@ -102,7 +102,7 @@ attribute 'dovecot/conf_files/imap',
 
 attribute 'dovecot/conf_files/pop3',
   :display_name => 'dovecot pop3 configuration files',
-  :description => 'Dovecot POP3 configuration files list',
+  :description => 'Dovecot POP3 configuration files list.',
   :type => 'array',
   :required => 'optional',
   :default => [
@@ -111,7 +111,7 @@ attribute 'dovecot/conf_files/pop3',
 
 attribute 'dovecot/conf_files/lmtp',
   :display_name => 'dovecot lmtp configuration files',
-  :description => 'Dovecot LMTP configuration files list',
+  :description => 'Dovecot LMTP configuration files list.',
   :type => 'array',
   :required => 'optional',
   :default => [
@@ -120,7 +120,7 @@ attribute 'dovecot/conf_files/lmtp',
 
 attribute 'dovecot/conf_files/sieve',
   :display_name => 'dovecot sieve configuration files',
-  :description => 'Dovecot Sieve configuration files list',
+  :description => 'Dovecot Sieve configuration files list.',
   :type => 'array',
   :required => 'optional',
   :default => [
@@ -130,7 +130,7 @@ attribute 'dovecot/conf_files/sieve',
 
 attribute 'dovecot/conf_files/ldap',
   :display_name => 'dovecot ldap configuration files',
-  :description => 'Dovecot LDAP configuration files list',
+  :description => 'Dovecot LDAP configuration files list.',
   :type => 'array',
   :required => 'optional',
   :default => [
@@ -140,21 +140,21 @@ attribute 'dovecot/conf_files/ldap',
 
 attribute 'dovecot/auth',
   :display_name => 'dovecot auth',
-  :description => 'Dovecot Authentication Databases as a hash of hashes. Supported authdbs: checkpassword, deny, ldap, master, passwdfile, sql, system and vpopmail',
+  :description => 'Dovecot Authentication Databases as a hash of hashes. Supported authdbs: checkpassword, deny, ldap, master, passwdfile, sql, system and vpopmail.',
   :type => 'string',
   :required => 'optional',
   :default => '{}'
 
 attribute 'dovecot/namespaces',
   :display_name => 'dovecot namespaces',
-  :description => 'Dovecot Namespaces as an array of hashes',
+  :description => 'Dovecot Namespaces as an array of hashes.',
   :type => 'array',
   :required => 'optional',
   :default => []
 
 attribute 'dovecot/plugins',
   :display_name => 'dovecot plugins',
-  :description => 'Dovecot Plugins configuration as a hash of hashes. Supported plugins: mail_log, acl and quota',
+  :description => 'Dovecot Plugins configuration as a hash of hashes. Supported plugins: mail_log, acl and quota.',
   :type => 'string',
   :required => 'optional',
   :default => '{
@@ -166,14 +166,14 @@ attribute 'dovecot/plugins',
 
 attribute 'dovecot/protocolos',
   :display_name => 'dovecot protocols',
-  :description => 'Dovecot Protocols configuration as a hash of hashes. Supported protocols: lda, imap, lmtp, sieve and pop3',
+  :description => 'Dovecot Protocols configuration as a hash of hashes. Supported protocols: lda, imap, lmtp, sieve and pop3.',
   :type => 'string',
   :required => 'optional',
   :default => '{}'
 
 attribute 'dovecot/services',
   :display_name => 'dovecot services',
-  :description => 'Dovecot Services configuration as a hash of hashes. Supported services: director, imap-login, pop3-login, lmtp, imap, pop3, auth, auth-worker, dict, tcpwrap, managesieve-login and managesieve',
+  :description => 'Dovecot Services configuration as a hash of hashes. Supported services: director, imap-login, pop3-login, lmtp, imap, pop3, auth, auth-worker, dict, tcpwrap, managesieve-login and managesieve.',
   :type => 'string',
   :required => 'optional',
   :default => '{}'
@@ -184,7 +184,7 @@ grouping 'dovecot/conf',
 
 attribute 'dovecot/conf/mail_plugins',
   :display_name => 'dovecot mail plugins',
-  :description => 'Dovecot default enabled mail_plugins',
+  :description => 'Dovecot default enabled mail_plugins.',
   :type => 'array',
   :required => 'optional',
   :default => []
