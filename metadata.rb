883f95d09708dab7128d0eba2a749e4a9f633084
Xabier de Zuazo <xabier@onddo.com>
2013-06-05 18:53:25 +0200

    metadata: added default recipe

--- metadata.rb
+++ metadata.rb
@@ -2,13 +2,15 @@ name             'dovecot'
 maintainer       'Onddo Labs, Sl.'
 maintainer_email 'team@onddo.com'
 license          'Apache 2.0'
-description      'Installs and configures dovecot.'
+description      'Installs and configures Dovecot.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 supports 'centos', '>= 6.0'
 supports 'debian', '>= 7.0'
 supports 'ubuntu', '>= 12.04'
+
+recipe 'dovecot::default', 'Installs and configures Dovecot.'
 
 attribute 'dovecot/user',
   :display_name => 'dovecot user',
