6cdc137a2de9460a91446c882c1f27d15841b98e
Xabier de Zuazo <xabier@onddo.com>
2013-06-06 20:32:42 +0200

    dovecot_test::attributes: dont test mailboxes

--- test/kitchen/cookbooks/dovecot_test/recipes/attributes.rb
+++ test/kitchen/cookbooks/dovecot_test/recipes/attributes.rb
@@ -87,36 +87,38 @@ node.default['dovecot']['namespaces'] = [
     'separator' => '/',
     'prefix' => '',
     'location' => 'maildir:~/Maildir',
-  }, {
-    'name' => 'inbox',
-    'separator' => '/',
-    'prefix' => '',
-    'inbox' => 'yes',
-    'inbox' => true,
-    'mailboxes' => {
-      'Drafts' => {
-        'special_use' => '\Drafts',
-      },
-      'Junk' => {
-        'special_use' => '\Junk',
-      },
-      'Trash' => {
-        'special_use' => '\Trash',
-      },
-      'Sent' => {
-        'special_use' => '\Sent',
-      },
-      'Sent Messages' => {
-        'special_use' => '\Sent',
-      },
-      'virtual/All' => {
-        'special_use' => '\All',
-      },
-      'virtual/Flagged' => {
-        'special_use' => '\All',
-      },
-    },
   },
+  # mailboxes require dovecot >= 2.1
+  # {
+  #   'name' => 'inbox',
+  #   'separator' => '/',
+  #   'prefix' => '',
+  #   'inbox' => 'yes',
+  #   'inbox' => true,
+  #   'mailboxes' => {
+  #     'Drafts' => {
+  #       'special_use' => '\Drafts',
+  #     },
+  #     'Junk' => {
+  #       'special_use' => '\Junk',
+  #     },
+  #     'Trash' => {
+  #       'special_use' => '\Trash',
+  #     },
+  #     'Sent' => {
+  #       'special_use' => '\Sent',
+  #     },
+  #     'Sent Messages' => {
+  #       'special_use' => '\Sent',
+  #     },
+  #     'virtual/All' => {
+  #       'special_use' => '\All',
+  #     },
+  #     'virtual/Flagged' => {
+  #       'special_use' => '\All',
+  #     },
+  #   },
+  # },
 ]
 
 # plugins.rb
