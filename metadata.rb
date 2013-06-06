6d1fc3d10f031c86a3ebaa8edb14b1dee7f2cf22
Xabier de Zuazo <xabier@onddo.com>
2013-06-06 21:23:46 +0200

    metadata: a more complete description

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'dovecot'
 maintainer       'Onddo Labs, Sl.'
 maintainer_email 'team@onddo.com'
 license          'Apache 2.0'
-description      'Installs and configures Dovecot.'
+description      'Installs and configures Dovecot, open source IMAP and POP3 email server.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
