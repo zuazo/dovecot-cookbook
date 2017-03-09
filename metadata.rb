64d947a5a12e61a83e6da5daa063d45a3b8367ee
Xabier de Zuazo <xabier@zuazo.org>
2017-03-09 15:57:48 +0100

    metadata: Add chef_version

--- metadata.rb
+++ metadata.rb
@@ -36,6 +36,8 @@ if respond_to?(:issues_url)
   issues_url "https://github.com/zuazo/#{name}-cookbook/issues"
 end
 
+chef_version '>= 12' if respond_to?(:chef_version)
+
 supports 'amazon'
 supports 'centos', '>= 6.0'
 supports 'debian', '>= 7.0'
