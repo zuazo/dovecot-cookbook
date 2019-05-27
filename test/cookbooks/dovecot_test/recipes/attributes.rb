873137a67e57dcebd3fc2e8cdb99c7fc2faea589
Edgaras Lukoševičius <edgaras.lukosevicius@gmail.com>
2019-05-27 17:14:45 +0300

    Dovecot 2.3 compatibility: director_doveadm_port -> inet_listener

--- test/cookbooks/dovecot_test/recipes/attributes.rb
+++ test/cookbooks/dovecot_test/recipes/attributes.rb
@@ -148,7 +148,8 @@ node.default['dovecot']['services']['director']['listeners'] =
   [
     { 'unix:login/director' => { 'mode' => '0666' } },
     { 'fifo:login/proxy-notify' => { 'mode' => '0666' } },
-    { 'unix:director-userdb' => { 'mode' => '0666' } }
+    { 'unix:director-userdb' => { 'mode' => '0666' } },
+    { 'inet:director-doveadm' => { 'port' => '9091' } }
   ]
 if node['platform'] != 'centos' # Avoid SELinux error in CentOS
   node.default['dovecot']['services']['director']['listeners'][0]['inet'] =
